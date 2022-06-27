2bc21917f9afa7066204a220c8378223a46da254
SammyBloom <nwachiemi@gmail.com>
2022-06-27 05:08:35 +0100

    First commit with the necessary files created and a simple array of numbers already complete

---
+++ array.js
@@ -0,0 +1,4 @@
+// Question Number 20
+const arrayNumber = [1,2,3];
+
+console.log(arrayNumber);
---
+++ bubble_sort.js
@@ -0,0 +1,7 @@
+// Question Number 22
+function bubbleSort(array){
+    return array;
+}
+
+bubbleSort([1, 2, 4, 8, 245, 94, 123, 84, 895, 678, 497, 2, 975, 1, 1454]);
+
