85730080d2df1ba412523484f15c2abfd1a321f3
SammyBloom <nwachiemi@gmail.com>
2022-06-27 05:54:55 +0100

    Implemented bubble sort algorithm to a array of numbers and logged the sorted numbers to the console

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,7 +1,15 @@
 // Question Number 22
 function bubbleSort(array){
+
+    for(let i = 0; i < array.length - 1; i++){
+        for(let j = 0; j < array.length - 1 - i; j++){
+            if (array[j] > array[j + 1]) {
+                [array[j], array[j + 1]] = [array[j + 1], array[j]];
+            }
+        }
+    }
     return array;
 }
 
-bubbleSort([1, 2, 4, 8, 245, 94, 123, 84, 895, 678, 497, 2, 975, 1, 1454]);
+console.log(bubbleSort([1, 2, 4, 8, 245, 94, 123, 84, 895, 678, 497, 2, 975, 1, 1454]));
 
